9fe29e3512c45c322ba3b312f1e45afa6dd2f7c1
Florian Nücke <florian@nuecke.de>
2017-08-27 22:33:19 +0200

    Herp derp, tested the wrong particles, I'm a genius.

--- src/main/java/li/cil/bedrockores/client/render/ModelBedrockOre.java
+++ src/main/java/li/cil/bedrockores/client/render/ModelBedrockOre.java
@@ -3,11 +3,9 @@ package li.cil.bedrockores.client.render;
 import com.google.common.base.Function;
 import com.google.common.collect.ImmutableList;
 import li.cil.bedrockores.client.model.ModelLoaderBedrockOre;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.IBakedModel;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.vertex.VertexFormat;
-import net.minecraft.init.Blocks;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.IModel;
 import net.minecraftforge.common.model.IModelState;
@@ -20,6 +18,10 @@ import java.util.Collection;
 @SideOnly(Side.CLIENT)
 public enum ModelBedrockOre implements IModel {
     INSTANCE;
+
+    // --------------------------------------------------------------------- //
+
+    public static final ResourceLocation BEDROCK_LOCATION = new ResourceLocation("blocks/bedrock");
 
     // --------------------------------------------------------------------- //
     // IModel
@@ -37,7 +39,8 @@ public enum ModelBedrockOre implements IModel {
     @Override
     public IBakedModel bake(final IModelState state, final VertexFormat format, final Function<ResourceLocation, TextureAtlasSprite> bakedTextureGetter) {
         final IBakedModel mask = ModelLoaderBedrockOre.INSTANCE.getMaskModel().bake(state, format, bakedTextureGetter);
-        final TextureAtlasSprite particleTexture = Minecraft.getMinecraft().getBlockRendererDispatcher().getBlockModelShapes().getModelForState(Blocks.BEDROCK.getDefaultState()).getParticleTexture();
+        final TextureAtlasSprite particleTexture = bakedTextureGetter.apply(BEDROCK_LOCATION);
+        assert particleTexture != null;
         return new BakedModelBedrockOre(mask, particleTexture);
     }
 
