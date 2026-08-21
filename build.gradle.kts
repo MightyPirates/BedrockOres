2009948b99e29c26f74868a8fbb55ae7a4d288b2
Florian Nücke <florian@nuecke.de>
2026-08-22 00:28:19 +0200

    Bring publishing pipelines in line
    Also publish to the github pages maven we already use for buildroot.

--- build.gradle.kts
+++ build.gradle.kts
@@ -185,3 +185,4 @@ for (platform in enabledPlatforms.split(',')) {
 
 registerGameTestTask()
 registerLintTask()
+configureMavenPublishing(minecraftVersion, "https://github.com/fnuecke/BedrockOres")
